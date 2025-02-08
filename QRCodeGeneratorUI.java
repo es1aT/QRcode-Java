0ea2fc69a22417be4dc9872158b0d5595f9f825a
yuji.N <0mj3882j18.0c3k@au.com>
2025-02-08 17:36:35 +0900

    add colorButton

--- QRCodeGeneratorUI.java
+++ QRCodeGeneratorUI.java
@@ -1,6 +1,19 @@
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.image.BufferedImage;
+
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
 public class QRCodeGeneratorUI extends JFrame {
     private JTextField urlField;
@@ -12,9 +25,13 @@ public class QRCodeGeneratorUI extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
+        // 最上部パネル
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.Y_AXIS));
+
         // 入力フィールド
         JPanel inputPanel = new JPanel();
-        inputPanel.setLayout(new FlowLayout());
+        inputPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
         urlField = new JTextField(25);
         JButton generateButton = new JButton("QRコード生成");
 
@@ -22,13 +39,39 @@ public class QRCodeGeneratorUI extends JFrame {
         inputPanel.add(urlField);
         inputPanel.add(generateButton);
 
-        add(inputPanel, BorderLayout.NORTH);
+        // 色選択ボタン
+        JPanel colorPanel = new JPanel();
+        colorPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        Color foregroundColor = Color.BLACK;
+        Color backgroundColor = Color.WHITE;
+        JButton fgColorButton;
+        JButton bgColorButton;
+
+        fgColorButton = new JButton("コードの色");
+        bgColorButton = new JButton("背景色");
+
+        colorPanel.add(fgColorButton);
+        colorPanel.add(bgColorButton);
+
+        // 最上部のパネルに追加
+        topPanel.add(inputPanel);
+        topPanel.add(colorPanel);
+        add(topPanel, BorderLayout.NORTH);
 
         // QRコード表示用ラベル
         qrLabel = new JLabel("", SwingConstants.CENTER);
         add(qrLabel, BorderLayout.CENTER);
 
-        // ボタンのクリックイベント
+        // 色ボタンのクリックイベント
+        fgColorButton.addActionListener(e -> {
+            JColorChooser.showDialog(this, "コードの色を選択", foregroundColor);
+        });
+
+        bgColorButton.addActionListener(e -> {
+            JColorChooser.showDialog(this, "背景色を選択", backgroundColor);
+        });
+
+        // 生成ボタンのクリックイベント
         generateButton.addActionListener(e -> {
             try {
                 String url = urlField.getText();
