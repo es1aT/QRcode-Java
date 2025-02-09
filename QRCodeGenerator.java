43bfadf3ba6e062c36836bfe0d4e62c8c659e0b0
es1aT <es8aC.1@gmail.com>
2025-02-09 15:12:54 +0900

    Update QRCodeGenerator.java

--- QRCodeGenerator.java
+++ QRCodeGenerator.java
@@ -1,21 +1,17 @@
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageConfig;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.geom.Ellipse2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.EnumMap;
 import java.util.Map;
-import java.util.stream.IntStream;
 
 public final class QRCodeGenerator {
 
